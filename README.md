f2f11c45d0a482e8e12543e8785d3cbd10737f50
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-09-22 03:27:31 +0700

    randomised portfolio page done

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 ## Referensi Gambar Luar
 
 - Photo by <a href="https://unsplash.com/@50mmaniac?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Romero Souza</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+- Gambar-gambar diambil dari screenshot pekerjaan terdahulu dan diubah menggunakan Canva\
+- Gambar cover carousel halaman Portfolio dibuat menggunakan Canva
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -29,84 +29,104 @@
         </div>
     </nav>
 
+    {{-- None of the images are actually... 'real, meaningful' images; not enough time to rummage throughoout dubious named folders --}}
     <main class="bg-light">
         {{-- Top showcase; not really --}}
         <div class="container-fluid min-vh-100 d-flex align-items-center py-3">
             <div id="carouselGallery" class="carousel carousel-dark slide" data-ride="carousel">
                 <div class="carousel-indicators">
-                    <button type="button" data-target="#carouselGallery" data-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-target="#carouselGallery" data-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-target="#carouselGallery" data-slide-to="2" aria-label="Slide 3"></button>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-interval="10000">
-                        <img src="/img/Diluc.png" class="d-block w-100" alt="Diluc">
+                        <img src="/img/latest_works.png" class="d-block w-100 img-fluid" alt="latest_work1">
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Diluc</h5>
-                            <p>Diluc</p>
+                            <h5>Latest work in development project</h5>
                         </div>
                     </div>
                     <div class="carousel-item" data-interval="2000">
-                        <img src="/img/Kaeya.png" class="d-block w-100" alt="Kaeya">
+                        <img src="/img/latest_works.png" class="d-block w-100 img-fluid" alt="latest_work2">
                         <div class="carousel-caption d-none d-md-block">
-                            <h5>Kaeya</h5>
-                            <p>Kaeya</p>
+                            <h5>Latest work in design</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="/img/Nobody.png" class="d-block w-100" alt="Nobody">
+                        <img src="/img/latest_works.png" class="d-block w-100 img-fluid" alt="latest_work3">
                         <div class="carousel-caption d-none d-md-block">
                             <div class="text-bg-dark">
-                                <h5>Nobody</h5>
-                                <p>Nobody</p>
+                                <h5>Latest random work</h5>
                             </div>
                         </div>
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-target="#carouselGallery" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                    <span class="sr-only">Previous</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-target="#carouselGallery" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                    <span class="sr-only">Next</span>
                 </button>
             </div>
         </div>
 
+        <h1 class="display-4 text-center mt-5 mb-4">Gallery of Past Projects</h1>
+
         {{-- Gallery --}}
-        <div class="album pt-3 pb-5">
-            <div class="container">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="cards-paket">
-                    <?php
-                    $category = [0, 1, 2];
-                    // randomise
-                    foreach ($category as $cat) : ?>
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <img src="https://placeimg.com/640/480/nature" class="cover" alt="a random image">
-                            <div class="card-body">
-                                <h5 class="card-title">Title goes here
-                                    <span class="card-text">
-                                        <?php if ($cat == 0) : ?>
-                                            <span class="badge color-success">Design</span>
-                                        <?php elseif ($cat == 1) : ?>
-                                            <span class="badge color-warning">Project</span>
-                                        <?php elseif ($cat == 2) : ?>
-                                            <span class="badge color-basic">Others</span>
-                                        <?php endif ?>
-                                    </span>
-                                </h5>
-                                <p class="card-text text-muted">Made 2020</p>
-                                </div>
-                            </div>
+        <div class="container pb-5">
+            <div class="card-deck">
+            <?php
+            $count = 0;
+            $row = 3;
+
+            while ($row > 0)
+            {
+            ?>
+                <div class="row mb-3">
+                <?php
+                $category = [];
+
+                for ($i = 0; $i < 3; $i++)
+                {
+                    $rannum = rand(0, 2);
+                    $category[] = $rannum;
+                }
+
+                foreach ($category as $cat) :
+                $year = rand(15, 22);
+                ?>
+                    <div class="card shadow-sm">
+                        <img class="card-img-top" src="https://placeimg.com/640/480/nature" alt="A random image">
+                        <div class="card-body">
+                            <h5 class="card-title">Title
+                                <span class="card-text">
+                                    <?php if ($cat == 0) : ?>
+                                        <span class="badge badge-success">Design</span>
+                                    <?php elseif ($cat == 1) : ?>
+                                        <span class="badge badge-warning">Project</span>
+                                    <?php elseif ($cat == 2) : ?>
+                                        <span class="badge badge-danger">Others</span>
+                                    <?php endif ?>
+                                </span>
+                            </h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, eius aut iste accusamus ullam alias. Delectus, aperiam at quisquam temporibus officia corporis odit, sapiente quae porro eum autem? Velit, pariatur!</p>
                         </div>
+                    <div class="card-footer text-right">
+                        <small class="text-muted">Made in 20<?= $year ?></small>
                     </div>
-                    <?php endforeach ?>
+                    </div>
+                <?php
+                endforeach;
+                ?>
                 </div>
+            <?php
+                $row--;
+            }
+            ?>
             </div>
         </div>
-    </main>
 
     {{-- Footer --}}
     <footer class="pb-2 pt-3 bg-dark">
@@ -116,6 +136,5 @@
     </footer>
 
     <script src="/js/app.js"></script>
-    <script src='/js/script.js'></script>
   </body>
 </html>
